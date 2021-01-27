91e339e7aec8eacf51e43c1432f119156a808b59
Jérémie Piellard <jeremie.piellard@gmail.com>
2021-01-27 22:05:44 +0100

    Added description text

--- src/generate-page.ts
+++ src/generate-page.ts
@@ -4,11 +4,11 @@ import * as path from "path";
 import { Demopage } from "webpage-templates";
 
 const data = {
-    title: "Lines",
-    description: "Transformation of a picture into lines.",
+    title: "Threading",
+    description: "Transformation of a picture into string art.",
     introduction: [
-        "This is a simple tool that turns images into lines.",
-        "The result can be exported in the SVG format."
+        "This tool generates string art from any picture of your choice. Pegs are first placed on the frame, and then a single-color thread is repeatedly ran from peg to peg in a straight line. The stacked segments progressively recreate the original image. This process was popularized by Petros Vrellis.",
+        "The monochrome mode uses a single thread, while the color mode uses 3 distinct threads. The result can be exported in the SVG format."
     ],
     githubProjectName: "image-stylization-threading",
     additionalLinks: [],
