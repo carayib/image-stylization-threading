9d843e0e73a0e28786c1fbcc1f630841f5572a8c
Jérémie Piellard <jeremie.piellard@gmail.com>
2021-01-28 21:24:18 +0100

    Remove unused indicator

--- src/generate-page.ts
+++ src/generate-page.ts
@@ -25,10 +25,6 @@ const data = {
             id: "segments-count",
             label: "Segments count"
         },
-        {
-            id: "thread-length",
-            label: "Thread length"
-        },
     ],
     canvas: {
         width: 512,
